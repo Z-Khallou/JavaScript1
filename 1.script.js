6dc1dc98affd0b9479cb5da5e65265e6575a5fa6
Zouhir Khallou <z.khallou@outlook.com>
2026-09-18 11:02:49 +0200

    Ändrat från skärm till konsol.

--- 1.script.js
+++ 1.script.js
@@ -1,12 +1,6 @@
-// Hämta rutan från HTML-koden
-const skarmRuta = document.getElementById("triangel-ruta");
-
 let hashtag = ""; // Börja med en helt tom text
 
 for (let i = 1; i < 8; i++) {
   hashtag = hashtag + "#"; // Lägg till ett tecken i texten
-
-  // Istället för console.log, tryck in texten på skärmen.
-  // \n gör en radbrytning så att nästa rad hamnar under.
-  skarmRuta.innerText += hashtag + "\n";
+  console.log(hashtag);    // Skriv ut den växande texten
 }
